02e8e12e3d78eadb6d54147a4d8782d55315cd6d
Dhruv Shrivastava <98174871+shrivastavadhruv2000@users.noreply.github.com>
2023-12-22 20:49:09 +0530

    Add files via upload

---
+++ JAVASCRIPT_01/Keyword.js
@@ -0,0 +1,15 @@
+const x=44
+//x=33
+
+console.log("x=",x)
+
+var p=100
+{
+    //var p=200
+    let p=300
+}
+console.log("p=",p)
+
+console.log(g)//Usage
+//let g=10//Declaration
+var g//Declaration using var
---
+++ JAVASCRIPT_01/array.js
@@ -0,0 +1,39 @@
+let arr = [12,13,14]
+console.log(arr)
+
+arr.push(19,18,17)  // added at the end
+
+console.log(arr)
+
+
+console.log(arr.slice(1,3))
+
+// let x =arr.pop()
+// console.log("x=",x)
+
+arr.splice(2,3)
+console.log(arr)
+
+arr.splice(1,2,20,30)
+console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
